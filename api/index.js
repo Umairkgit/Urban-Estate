299cece8f59b88bf8550d81684accac3fb7a8eff
Umair Khan <umairk9530@gmail.com>
2024-08-28 22:21:45 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { error } from 'console';
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB!');
+}).catch((err) => {
+    console.log(err);
+});
+
+
 
 const app = express();
 
 app.listen(3000, () => {
     console.log("Server in listening on port 3000");
-    }
+}
 );
