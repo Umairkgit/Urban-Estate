c0baee2e551197dd1684f4b0c2c2c6fe8f902070
Umair Khan <umairk9530@gmail.com>
2024-08-31 18:33:50 +0530

    signin page func

--- Estate/src/pages/SignUp.jsx
+++ Estate/src/pages/SignUp.jsx
@@ -58,7 +58,7 @@ export default function SignUp() {
 
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
-        <Link to={"/sign-up"}>
+        <Link to={"/sign-in"}>
           <span className='text-blue-700 '>Sign in</span>
         </Link>
       </div>
